1e4f55c3e7cc93ff70d98f113a87180654a5a6a1
Tigran7 <tarakelov@icloud.com>
2022-05-31 12:00:54 -0700

    context

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-from Back.models.models import db
+from backend.models.models import db
 
 from routes.auth import auth
 from routes.posts import posts
